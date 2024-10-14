5c812c6cf3a8d6e5e434ec0db8e1cac8d6740477
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-10-14 17:26:49 +0800

    add factory & seeder

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+
+class ContactSeeder extends Seeder
+{
+    public function run()
+    {
+        //50 ka generated accounts
+        Contact::factory()->count(50)->create();
+    }
+}
